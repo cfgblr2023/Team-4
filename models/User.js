431d1fab1403c49ecd20a3f0b019e54239bfa3af
aashuu1856 <aashuu1856@gmail.com>
2023-07-01 23:54:46 +0530

    Schema Changed for Organisation

--- models/User.js
+++ models/User.js
@@ -23,7 +23,7 @@ const UserSchema = new mongoose.Schema({
     },
     role:{
     type: String,
-    enum: ['user','admin','org'],
+    enum: ['user','admin'],
     default: 'user',
     required: true
     }
